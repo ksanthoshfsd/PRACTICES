40bac5b4a5d05a714cedd5204c722242c5b09e27
ksanthoshfsd <ksanthoshfsd@gmail.com>
2024-03-21 23:59:19 +0530

    practice

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var Resume = {
+    hi : "sn",
+    age : 20,
+    gender = " male",
+    skills = ["js", "html", "css"],
+    education qualification = [10,12,"full stack developer"]
+}
+console.log(Resume)
